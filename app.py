287d2c705585edbcbc7914cd9379b048a156f91a
Suryadev2811 <devs02819@gmail.com>
2026-04-03 15:30:11 +0530

    added model download

--- app.py
+++ app.py
@@ -198,14 +198,30 @@ RISK_COLORS = {"Good": "#10b981", "AtRisk": "#f59e0b", "Critical": "#ef4444"}
 # ─────────────────────────────────────────
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+import requests
+
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1lrxOFCTAuu8Im6aGwM8xawkBXLweWvz9"
+MODEL_PATH = os.path.join(BASE_DIR, "rf_model.joblib")
+
 @st.cache_resource
 def load_artifacts():
-    model   = joblib.load(os.path.join(BASE_DIR, "rf_model.joblib"))
+    if not os.path.exists(MODEL_PATH):
+        with st.spinner("⬇ Downloading ML model... please wait"):
+            response = requests.get(MODEL_URL, stream=True)
+
+            if response.status_code == 200:
+                with open(MODEL_PATH, "wb") as f:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        f.write(chunk)
+            else:
+                raise Exception("❌ Model download failed. Check Google Drive link permissions.")
+
+    model = joblib.load(MODEL_PATH)
     encoder = joblib.load(os.path.join(BASE_DIR, "label_encoder.joblib"))
+
     return model, encoder
 
 rf_model, label_encoder = load_artifacts()
-
 FEATURES = [
     'attendance_pct',
     'quiz_1','quiz_2','quiz_3','quiz_4','quiz_5',
